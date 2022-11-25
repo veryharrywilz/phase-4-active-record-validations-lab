56095d290ce3469e7f7c9a8d7e2deace62db5da6
veryharrywilz <110489443+veryharrywilz@users.noreply.github.com>
2022-11-25 13:25:59 -0600

    commit tree 2a445c2a9d31817a3c71d81edb2446ccfb65ce94

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,26 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: {maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+    validate :clickbait?
+
+
+
+    def clickbait?
+        clickbait_array = [/Won't Believe/i, /Secret/i, /Top \d/i, /Guess/i]
+          clickbait_counter = 0 
+          clickbait_array.each do |term|
+                if self.title.includes(term)
+                    clickbait_counter += 1
+                 end
+            end
+            if clickbait_counter > 0
+                errors.add(:title, "must be clickbait")
+            end
+    end
+
+
+
 end
